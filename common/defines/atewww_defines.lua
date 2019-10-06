60971e0e0fc8a9a57421e71278ae466fed00fdb3
Antoni Baum <antoni.baum@gmail.com>
2019-10-06 14:32:27 +0200

    Various fixes
    Presidential event chain
    Laurentides starting with no castle
    Error log fixes
    Cetic imperial ultimatum fixes

--- common/defines/atewww_defines.lua
+++ common/defines/atewww_defines.lua
@@ -1,7 +1,2 @@
 NDefines.NDiplomacy.JAIN_LIEGE_OPINION_BONUS = 0								-- Jain Lieges Get Vassal Opinion Bonus
-NDefines.NCharacter.FERTILITY_BASE_MULT = 0.40
-NDefines.NCharacter.SECONDARY_SPOUSE_FERTILITY_MULT = 0.25
-NDefines.NEngine.EVENT_PROCESS_OFFSET = 40
-NDefines.NEngine.COURTIER_EVENT_PROCESS_OFFSET = 100
-NDefines.NEngine.COURT_PRUNE_SIZE = 5
-NDefines.NEngine.PRUNE_MINIMAL_AGE = 1
+NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 2666
